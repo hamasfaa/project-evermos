c3dd7724ca6cffed4b0b53008678b99b9a4ed1e0
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-07-22 19:32:47 +0700

    feat(middleware): implement JWT authentication middleware with success and error handling

---
+++ middleware/jwt.go
@@ -0,0 +1,52 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/hamasfaa/project-evermos/configuration"
+	"github.com/hamasfaa/project-evermos/model"
+)
+
+func AuthenticateJWT(requireAdmin bool, config configuration.Config) func(*fiber.Ctx) error {
+	jwtSecret := config.Get("JWT_SECRET_KEY")
+	return jwtware.New(jwtware.Config{
+		SigningKey:  []byte(jwtSecret),
+		TokenLookup: "header:token",
+		SuccessHandler: func(ctx *fiber.Ctx) error {
+			user := ctx.Locals("user").(*jwt.Token)
+			claims := user.Claims.(jwt.MapClaims)
+			noTelp := claims["noTelp"].(string)
+			isAdmin := claims["is_admin"].(bool)
+
+			ctx.Locals("noTelp", noTelp)
+			ctx.Locals("is_admin", isAdmin)
+
+			if requireAdmin && !isAdmin {
+				return ctx.Status(fiber.StatusUnauthorized).JSON(model.GeneralResponse{
+					Code:    401,
+					Message: "Unauthorized",
+					Data:    "Admin only access",
+				})
+			}
+
+			return ctx.Next()
+		},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			if err.Error() == "Missing or malformed JWT" {
+				return c.Status(fiber.StatusBadRequest).JSON(model.GeneralResponse{
+					Code:    400,
+					Message: "Bad Request",
+					Data:    "Missing or malformed JWT",
+				})
+			} else {
+				return c.Status(fiber.StatusUnauthorized).JSON(model.GeneralResponse{
+					Code:    401,
+					Message: "Unauthorized",
+					Data:    "Invalid or expired JWT",
+				})
+			}
+		},
+	})
+
+}
