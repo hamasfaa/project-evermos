ae752c05f1e128399d1f2d9fd4d8177db037b8fb
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-07-23 19:05:08 +0700

    fix(user): update Create method signature to return user ID and error

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type UserRepository interface {
-	Create(ctx context.Context, user *entity.User) error
+	Create(ctx context.Context, user *entity.User) (int, error)
 	GetByPhone(ctx context.Context, phone string, password string) (*entity.User, error)
 	Me(ctx context.Context, noTelp string) (*entity.User, error)
 	Update(ctx context.Context, noTelp string, user *entity.User) error
