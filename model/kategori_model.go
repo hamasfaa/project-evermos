db33032fb50bd4adcba3b4fec42d5a175f2c63e7
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-07-24 20:43:31 +0700

    feat(kategori): add KategoriResponse struct for enhanced category data representation

--- model/kategori_model.go
+++ model/kategori_model.go
@@ -3,3 +3,8 @@ package model
 type Kategori struct {
 	NamaKategori string `json:"nama_category"`
 }
+
+type KategoriResponse struct {
+	ID           int    `json:"id"`
+	NamaKategori string `json:"nama_category"`
+}
