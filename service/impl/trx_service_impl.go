95ca84e71a67a60fc4f9a4540d65413e4f17c498
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-07-30 21:32:30 +0700

    feat(transaction): enhance CreateTransaction to fetch product details and validate stock availability

--- service/impl/trx_service_impl.go
+++ service/impl/trx_service_impl.go
@@ -3,6 +3,7 @@ package impl
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/hamasfaa/project-evermos/entity"
@@ -11,22 +12,48 @@ import (
 	"github.com/hamasfaa/project-evermos/service"
 )
 
-func NewTrxServiceImpl(trxRepository *repository.TrxRepository) service.TrxService {
-	return &trxServiceImpl{TrxRepository: *trxRepository}
+func NewTrxServiceImpl(trxRepository *repository.TrxRepository, productRepository *repository.ProductRepository) service.TrxService {
+	return &trxServiceImpl{TrxRepository: *trxRepository, ProductRepository: *productRepository}
 }
 
 type trxServiceImpl struct {
 	repository.TrxRepository
+	repository.ProductRepository
 }
 
 func (s *trxServiceImpl) CreateTransaction(ctx context.Context, userID int, transaction *model.Transaksi) error {
+	productData := make(map[int]*entity.Produk)
+	var totalHarga int
+
+	for _, detail := range transaction.DetailTrx {
+		if _, exist := productData[detail.ProductID]; !exist {
+			product, err := s.ProductRepository.GetByID(ctx, detail.ProductID)
+			if err != nil {
+				return err
+			}
+			productData[detail.ProductID] = product
+		}
+
+		product := productData[detail.ProductID]
+
+		if product.Stok < detail.Kuantitas {
+			return fmt.Errorf("insufficient stock for product %s. Available: %d, Requested: %d",
+				product.NamaProduk, product.Stok, detail.Kuantitas)
+		}
+
+		hargaKonsumen, _ := strconv.Atoi(product.HargaKonsumen)
+
+		totalHarga += hargaKonsumen * detail.Kuantitas
+	}
+
 	kodeInvoice := fmt.Sprintf("INV-%d-%d", userID, time.Now().Unix())
 
 	entityTransaction := &entity.Trx{
-		UserID:   userID,
-		AlamatID: transaction.AlamatKirim,
-		Kode:     kodeInvoice,
-		Metode:   transaction.Metode,
+		UserID:     userID,
+		AlamatID:   transaction.AlamatKirim,
+		Kode:       kodeInvoice,
+		Metode:     transaction.Metode,
+		HargaTotal: totalHarga,
 	}
 
 	id, err := s.TrxRepository.CreateTransaction(ctx, entityTransaction)
@@ -37,16 +64,31 @@ func (s *trxServiceImpl) CreateTransaction(ctx context.Context, userID int, tran
 	entityTransaction.DetailTrx = make([]entity.DetailTrx, len(transaction.DetailTrx))
 
 	for i, detail := range transaction.DetailTrx {
+		product := productData[detail.ProductID]
+		hargaKonsumen, _ := strconv.Atoi(product.HargaKonsumen)
+
+		hargaTotal := hargaKonsumen * detail.Kuantitas
+
 		entityTransaction.DetailTrx[i] = entity.DetailTrx{
-			TrxID:     id,
-			ProdukID:  detail.ProductID,
-			Kuantitas: detail.Kuantitas,
+			TrxID:      id,
+			ProdukID:   detail.ProductID,
+			Kuantitas:  detail.Kuantitas,
+			HargaTotal: hargaTotal,
 		}
 	}
 
 	_, err = s.TrxRepository.CreateDetailTransaction(ctx, entityTransaction.DetailTrx)
 	if err != nil {
 		return err
+	}
+
+	for _, detail := range entityTransaction.DetailTrx {
+		product := productData[detail.ProdukID]
+		product.Stok -= detail.Kuantitas
+		err := s.ProductRepository.UpdateStock(ctx, product.ID, product.Stok)
+		if err != nil {
+			return fmt.Errorf("failed to update stock for product %s: %w", product.NamaProduk, err)
+		}
 	}
 
 	return nil
